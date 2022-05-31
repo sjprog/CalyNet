ca82ea6a57de436a5c9e1fd5e5fdbd37b2b700fe
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-05-31 09:18:47 -0300

    organizando pastas e rotas

---
+++ database/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require("sequelize");
+
+const connection = new Sequelize('CalyNet', 'root', 'Si96513782', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+module.exports = connection;
