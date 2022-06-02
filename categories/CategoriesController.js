54e7a5ff780402293160572529377977474f61e4
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-06-02 14:53:37 -0300

    POST

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -12,7 +12,7 @@ router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new");
 });
 
-router.get("/categories/save", (req, res) => {
+router.post("/categories/save", (req, res) => {
     var title = req.body.title;
     if (title != undefined) {    
 
