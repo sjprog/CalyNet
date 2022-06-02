24b5032d4ba91e4d501e6492d206fe939d984ca9
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-06-02 14:50:14 -0300

    slugify

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { is } = require("express/lib/request");
 const router = express.Router();
+const Category = require("./Category");
+const slugify = require("slugify"); 
 
 router.get("/categories", (req, res) => {
     res.send("ROTA DE CATEGORIAS!")
@@ -7,6 +10,22 @@ router.get("/categories", (req, res) => {
 
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new");
+});
+
+router.get("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if (title != undefined) {    
+
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/");
+        })
+        
+    } else {
+        res.redirect("/admin/categories/new");
+    }
 });
 
 module.exports = router;
