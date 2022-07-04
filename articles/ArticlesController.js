6fc35797c4a5f7d188b4fac734d962921ee93f5b
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-07-04 10:06:42 -0300

    article/save

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
 router.get("/articles", (req, res) => {
     res.send("ROTA DE ARTIGOS!")
@@ -7,6 +10,23 @@ router.get("/articles", (req, res) => {
 
 router.get("/admin/articles/new", (req, res) => {
     res.render("admin/articles/new")
+});
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var Category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+
+    });
+
+
+
 });
 
 module.exports = router;
